bb26ba3423a22313dfc9b0f98212b9b49bd92ee7
andy <huayu.cao@pintec.com>
2019-12-24 11:04:48 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-// import './test/strStr'
-import './adventofcode/amplify'
+import './adventofcode/spaceImageFormat'
 class App extends React.Component {
-
     render(){
         return (<div>
             <p>main</p>
         </div>)
     }
 }
-
-
 
 export default App;
