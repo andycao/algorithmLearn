d3d091e357454f8e212e44c75ac9fdcc84bdbad7
Andy Cao <cao_huayu@hotmail.com>
2020-02-28 23:19:06 +0800

    after eject

---
+++ doc/precache-manifest.05d9d2c1afdfad8d769f93486aad1f72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1927c124da4b77f5f21ef613550306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88b37afe20f3bf8b82f0",
+    "url": "/static/css/main.38a94ffd.chunk.css"
+  },
+  {
+    "revision": "ca93f4cdc39d62d30374",
+    "url": "/static/js/2.73e44033.chunk.js"
+  },
+  {
+    "revision": "88b37afe20f3bf8b82f0",
+    "url": "/static/js/main.d130141a.chunk.js"
+  },
+  {
+    "revision": "63412404cc7c4961ba22",
+    "url": "/static/js/runtime-main.31f8f952.js"
+  }
+]);
